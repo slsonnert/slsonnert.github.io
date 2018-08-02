b7f1180f6a06d194c8857b9847c631dee50da67b
gwc14 <gwc14@gwc14s-MacBook-Air.local>
2018-08-02 15:36:39 -0400

    Initial commit

--- myScript.js
+++ myScript.js
@@ -1,2 +1,13 @@
 
-document.getElementById("IDname")
+
+var i = 0
+var txt = 'Small things to change to help the environment';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("k").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter,speeed);
+  }
+}
